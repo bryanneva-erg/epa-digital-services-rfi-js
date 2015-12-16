49a3e325266a9377a428be3718c9ec53ce791950
Bryan Neva <bryan.neva@gmail.com>
2015-12-15 23:35:20 -0500

    Debug prodServer.js for Heroku

--- prodServer.js
+++ prodServer.js
@@ -5,10 +5,14 @@ var config = require('./webpack.config.prod');
 
 var app = express();
 var compiler = webpack(config);
+var static_path = path.join(__dirname,'dist');
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.use(express.static(static_path))
+    .get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, 'index.html'), {
+            root: static_path
+        });
+    });
 
 var isProduction = process.env.NODE_ENV === 'production';
 var port = isProduction ? process.env.PORT : 3000;
