f1209e1b8192ccba56ee7def1977840d3190e9b6
Bryan Neva <bryan.neva@gmail.com>
2015-12-15 23:01:08 -0500

    Add production server for debug purposes

---
+++ prodServer.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var express = require('express');
+var webpack = require('webpack');
+var config = require('./webpack.config.prod');
+
+var app = express();
+var compiler = webpack(config);
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+var isProduction = process.env.NODE_ENV === 'production';
+var port = isProduction ? process.env.PORT : 3000;
+// var publicPath = path.resolve(__dirname, 'public');
+
+app.listen(port, 'localhost', function(err) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log('Listening at http://localhost:3000');
+});
