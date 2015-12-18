525c1e7f6eaeebdef6c517b46747814027c1d5b7
Bryan Neva <bryan.neva@gmail.com>
2015-12-18 11:51:59 -0500

    Removed rendering containers from index.html and instead render them with react. - Remove app components from index.js and App.js and put them into their own files - Update App.js to import components appropriately

--- src/App.js
+++ src/App.js
@@ -1,39 +1,54 @@
 import React, { Component } from 'react';
+import { Counter } from './assets/scripts/components/Counter';
 import { NICE, SUPER_NICE } from './colors';
+import LatLng from './assets/scripts/components/leaflet/LatLng.js';
+import { LineGraph } from './assets/scripts/components/graph/LineGraph.js';
+import './assets/styles/base.scss';
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { counter: 0 };
-    this.interval = setInterval(() => this.tick(), 1000);
-  }
-
-  tick() {
-    this.setState({
-      counter: this.state.counter + this.props.increment
-    });
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  render() {
-    return (
-      <h1 style={{ color: this.props.color }}>
-        Counter ({this.props.increment}): {this.state.counter}
-      </h1>
-    );
-  }
-}
+const example_data = [{
+    "year":2005,
+    "cumulative_so2":24446
+},{
+    "year":2006,
+    "cumulative_so2":34446
+},{
+    "year":2007,
+    "cumulative_so2":34446
+},{ 
+    "year":2008,
+    "cumulative_so2":44446
+},{
+    "year":2009,
+    "cumulative_so2":54446
+},{
+    "year":2010,
+    "cumulative_so2":64446
+},{
+    "year":2011,
+    "cumulative_so2":75284
+},{
+    "year":2012,
+    "cumulative_so2":76284
+},{
+    "year":2013,
+    "cumulative_so2":76384
+},{
+    "year":2014,
+    "cumulative_so2":76784
+}];
 
 export class App extends Component {
   render() {
     return (
-      <div>
-        <Counter increment={1} color={NICE} />
-        <Counter increment={5} color={SUPER_NICE} />
-      </div>
+        <div>
+            <Counter increment={1} color={NICE} />
+            <Counter increment={5} color={SUPER_NICE} />
+
+            <div id="line-graph-container"><LineGraph data={example_data} /></div>
+            <div id="map"><LatLng /></div>
+            
+        </div>
+        
     );
   }
 }
