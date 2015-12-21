3f9e307dd9095f6d76f0e8af5fab03dc7df1a3b0
Bryan Neva <bryan.neva@gmail.com>
2015-12-20 21:16:37 -0500

    Add FL to cached SO2 data

--- src/assets/data/AMBIENT_SO2_CACHE.js
+++ src/assets/data/AMBIENT_SO2_CACHE.js
@@ -1,54 +1,27 @@
 const AMBIENT_SO2_CACHE = {
-    'AK':{
-        2005: {
-            pollutant: 'Sulfur dioxide',
-            emissions: '7203',
-            unit: 'Pounds';
-        },
-        2006: {
-            pollutant: 'Sulfur dioxide',
-            emissions: '7301',
-            unit: 'Pounds';
-        },
-        2007: {
-            pollutant: 'Sulfur dioxide',
-            emissions: '9263',
-            unit: 'Pounds';
-        },
-        2008: {
-            pollutant: 'Sulfur dioxide',
-            emissions: '10008',
-            unit: 'Pounds';
-        },
-        2009: {
-            pollutant: 'Sulfur dioxide',
-            emissions: '7712',
-            unit: 'Pounds';
-        },
-        2010: {
-            pollutant: 'Sulfur dioxide',
-            emissions: '10476',
-            unit: 'Pounds';
-        },
-        2011: {
-            pollutant: 'Sulfur dioxide',
-            emissions: '11058',
-            unit: 'Pounds';
-        },
-        2012: {
-            pollutant: 'Sulfur dioxide',
-            emissions: '10242',
-            unit: 'Pounds';
-        },
-        2013: {
-            pollutant: 'Sulfur dioxide',
-            emissions: '9933',
-            unit: 'Pounds';
-        },
-        2014: {
-            pollutant: 'Sulfur dioxide',
-            emissions: '8070',
-            unit: 'Pounds';
-        }
-    }
+    'AK': {
+        2005: { pollutant: 'Sulfur dioxide', emissions: '7203', unit: 'Pounds'; },
+        2006: { pollutant: 'Sulfur dioxide', emissions: '7301', unit: 'Pounds'; },
+        2007: { pollutant: 'Sulfur dioxide', emissions: '9263', unit: 'Pounds'; },
+        2008: { pollutant: 'Sulfur dioxide', emissions: '10008', unit: 'Pounds'; },
+        2009: { pollutant: 'Sulfur dioxide', emissions: '7712', unit: 'Pounds';  },
+        2010: { pollutant: 'Sulfur dioxide', emissions: '10476', unit: 'Pounds'; },
+        2011: { pollutant: 'Sulfur dioxide', emissions: '11058', unit: 'Pounds'; },
+        2012: { pollutant: 'Sulfur dioxide', emissions: '10242', unit: 'Pounds'; },
+        2013: { pollutant: 'Sulfur dioxide', emissions: '9933', unit: 'Pounds'; },
+        2014: { pollutant: 'Sulfur dioxide', emissions: '8070', unit: 'Pounds'; }
+    },
+    'FL': {
+        2005: { pollutant: 'Sulfur dioxide', emissions: '10922', unit: 'Pounds'},
+        2006: { pollutant: 'Sulfur dioxide', emissions: '13261', unit: 'Pounds'},
+        2007: { pollutant: 'Sulfur dioxide', emissions: '12986', unit: 'Pounds'},
+        2008: { pollutant: 'Sulfur dioxide', emissions: '6540', unit: 'Pounds'},
+        2009: { pollutant: 'Sulfur dioxide', emissions: '5003', unit: 'Pounds'},
+        2010: { pollutant: 'Sulfur dioxide', emissions: '4091', unit: 'Pounds'},
+        2011: { pollutant: 'Sulfur dioxide', emissions: '4701', unit: 'Pounds'},
+        2012: { pollutant: 'Sulfur dioxide', emissions: '5789', unit: 'Pounds'},
+        2013: { pollutant: 'Sulfur dioxide', emissions: '7010', unit: 'Pounds'},
+        2014: { pollutant: 'Sulfur dioxide', emissions: '5183', unit: 'Pounds'}
+    },
+    'CO' {}
 }
